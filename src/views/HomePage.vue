20284b2444b5441c3c6f8bfd1d0de24c9f2d2d96
Andres Pizana <apizana1@stu.southtexascollege.edu>
2025-03-09 22:11:48 -0500

    added style to categories

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -14,7 +14,15 @@ onMounted(async () => {
 
 <template>
   <BaseTitle>TRIVIA APPLICATION</BaseTitle>
-  <div v-for="category in categories" :key="category.id">
-    {{ category.name }}
+  <div class="grid flex-grow grid-cols-4 gap-12 m-20">
+      <RouterLink v-for="category in categories" 
+      :key="category.id"
+      :to="``"
+      class="bg-white text-center flex h-32 items-center justify-center rounded-lg border-4 border-slate-300 py-4 font-bold uppercase hover:cursor-pinter hover:border-black hover:bg-pink-300 hover:text-white transition-colors duration-250"
+      >
+        {{ category.name }}
+      </RouterLink>
   </div>
+
+  
 </template>
